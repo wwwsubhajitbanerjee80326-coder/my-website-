a1b09ee6dd19e61cf8cf248d4623349b929ca1ab
wwwsubhajitbanerjee80326-coder <www.subhajitbanerjee80326@gmail.com>
2025-12-30 20:02:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server started");
+});
+
 
 app.use(express.json());
 app.use(express.static("public"));
